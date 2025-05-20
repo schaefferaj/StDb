182f961629f7fa14274865682b0b08c38b84338c
Pascal Audet <pascal.audet@uottawa.ca>
2025-05-20 21:19:15 +1200

    view source code in docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ from stdb import __version__ as release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages', 'sphinx.ext.viewcode']
 
 autodoc_member_order = 'bysource'
 
